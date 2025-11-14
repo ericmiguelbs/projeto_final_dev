88a7c59718faa36e14383b9271d6de0f04d94c85
eric silva <ericmiguelbdasilva.com@gmail.com>
2025-11-14 19:47:02 -0300

    Ajustando comandos

--- test_app.py
+++ test_app.py
@@ -2,7 +2,6 @@ import unittest
 from app import app
 import werkzeug
 
-# Patch temporário para werkzeug
 if not hasattr(werkzeug, '__version__'):
     werkzeug.__version__ = "mock-version"
 
@@ -12,18 +11,18 @@ class CriticalTests(unittest.TestCase):
     def setUpClass(cls):
         cls.client = app.test_client()
 
-    # 1 — Token inválido → deve recusar
+    #Token inválido
     def test_protected_invalid_token(self):
         headers = {"Authorization": "Bearer token_invalido_123"}
         response = self.client.get('/protected', headers=headers)
         self.assertEqual(response.status_code, 422)
 
-    # 2 — Método incorreto → 405
+    #Método incorreto
     def test_items_wrong_method(self):
         response = self.client.post('/items')
         self.assertEqual(response.status_code, 405)
 
-    # 3 — Estrutura do token no login
+    # 3Estrutura do token no login
     def test_login_token_structure(self):
         response = self.client.post('/login')
         self.assertEqual(response.status_code, 200)
